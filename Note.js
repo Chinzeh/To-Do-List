c8e8934a79735a287ef7824d9d58931953998500
Chinzeh <85343987+Chinzeh@users.noreply.github.com>
2022-04-04 11:42:43 +0100

    Add files via upload

---
+++ Note.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Css/Note.css';
+import {Checkbox} from '@material-ui/core';
+
+function Note(props) {
+    function removeNote() {
+      props.remove(props.id);
+    }
+    return (
+      <div className={`note ${props.class}`}>
+        <Checkbox checked={false} value="checkedA" onClick={removeNote} />
+        <h3 className="msg">{props.msg}</h3>
+      </div>
+    );
+  }
+  
+  export default Note;
+  
